ba3af2c0511122aea08fd9394701f6b608f41ccf
CoreyShupe <coreyshupe101@gmail.com>
2021-10-10 08:50:16 -0400

    Fix some nits.

--- velocity/src/main/java/com/github/coreyshupe/lb/velocity/listeners/VelocityInitialConnectListener.java
+++ velocity/src/main/java/com/github/coreyshupe/lb/velocity/listeners/VelocityInitialConnectListener.java
@@ -1,21 +1,18 @@
 package com.github.coreyshupe.lb.velocity.listeners;
 
 import com.github.coreyshupe.lb.api.LoadBalancerInstance;
-import com.velocitypowered.api.event.Continuation;
+import com.velocitypowered.api.event.EventTask;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
 
 public record VelocityInitialConnectListener(ProxyServer proxyServer, LoadBalancerInstance instance) {
     @Subscribe
-    public void onInitialConnect(PlayerChooseInitialServerEvent event, Continuation continuation) {
-        this.instance.getServerBalanceCache().getBestServer().whenComplete((bestServer, throwable) -> {
-            if (throwable != null) {
-                continuation.resumeWithException(throwable);
-            } else if (bestServer != null) {
+    public EventTask onInitialConnect(PlayerChooseInitialServerEvent event) {
+        return EventTask.resumeWhenComplete(this.instance.getServerBalanceCache().getBestServer().whenComplete((bestServer, throwable) -> {
+            if (bestServer != null) {
                 this.proxyServer.getServer(bestServer).ifPresent(event::setInitialServer);
-                continuation.resume();
             }
-        });
+        }));
     }
 }
--- velocity/src/main/java/com/github/coreyshupe/lb/velocity/listeners/VelocityLoadBalancerReloadListener.java
+++ velocity/src/main/java/com/github/coreyshupe/lb/velocity/listeners/VelocityLoadBalancerReloadListener.java
@@ -10,13 +10,11 @@ import java.io.IOException;
 
 public record VelocityLoadBalancerReloadListener(Logger logger, LoadBalancerInstance instance) {
     @Subscribe
-    public void onReload(@SuppressWarnings("unused") ProxyReloadEvent unused, Continuation continuation) {
+    public void onReload(@SuppressWarnings("unused") ProxyReloadEvent unused) {
         try {
             this.instance.getConfigProvider().reloadConfig();
-            continuation.resume();
         } catch (IOException error) {
             this.logger.error("Failed to reload VelocityLoadBalancer.", error);
-            continuation.resumeWithException(error);
         }
     }
 }
